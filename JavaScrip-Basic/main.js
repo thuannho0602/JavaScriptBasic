8d81f18499c33fadd4a9915289bf69da57e7e52e
thuannguyen <thuannho0602@gmail.com>
2023-06-01 16:21:42 +0700

    toán tu

--- JavaScrip-Basic/main.js
+++ JavaScrip-Basic/main.js
@@ -1,39 +1,39 @@
-// Khai báo Biến
-// var fullName='Nguyen Minh THuan';
-// var age=23
+// // Khai báo Biến
+// // var fullName='Nguyen Minh THuan';
+// // var age=23
 
-// // Gọi Hàm Thông Báo 
-// alert(fullName);
-// alert(age)
-
-
-/** Giới Thiệu Một số built-in
- * 1.Alert
- * 2.Console, console.log('') 
- * 3.Confirm
- * 4.Prompt
- * 5.Set Timeout
- * 6.Set Interval
- */
-
-var fullName=('THuận nhỏ');
-
-console.error(fullName)
-
-// // Dùng Để xác Nhận
-confirm('Xác Nhận đủ tuổi')
+// // // Gọi Hàm Thông Báo 
+// // alert(fullName);
+// // alert(age)
 
 
-// // dùng để xác nhận và có text đễ người dùng nhập vào
-prompt('Xác Nhận đủ tuổi')
+// /** Giới Thiệu Một số built-in
+//  * 1.Alert
+//  * 2.Console, console.log('') 
+//  * 3.Confirm
+//  * 4.Prompt
+//  * 5.Set Timeout
+//  * 6.Set Interval
+//  */
 
-// dùng để chuyền 1 function và thời gian và giá trị thời gian đươc tín bằng mili giây
-setTimeout(function() {
-    alert('thông báo')
-},2000)// 2 giây
+// var fullName=('THuận nhỏ');
 
-// 
-setInterval(function() {
-    console.log('Day Là Log'+Math.random)
-},2000)// 2 giây
+// console.error(fullName)
+
+// // // Dùng Để xác Nhận
+// confirm('Xác Nhận đủ tuổi')
+
+
+// // // dùng để xác nhận và có text đễ người dùng nhập vào
+// prompt('Xác Nhận đủ tuổi')
+
+// // dùng để chuyền 1 function và thời gian và giá trị thời gian đươc tín bằng mili giây
+// setTimeout(function() {
+//     alert('thông báo')
+// },2000)// 2 giây
+
+// // 
+// setInterval(function() {
+//     console.log('Day Là Log'+Math.random)
+// },2000)// 2 giây
 
---
+++ JavaScrip-Basic/mainToanTu.js
@@ -0,0 +1,32 @@
+/**Toán Tử
+ * 1 toán tử số hoc
+ * 2 toán tử gán
+ * 3 toán tử so sanh
+ * 4 toán tử logic
+ * 
+ */
+
+//1 toán tử số học
+var a=1+2;
+console.log(a);
+
+//2 toán tử gán
+var fullName='Nguyễn Minh Thuận'
+console.log(fullName);
+
+//3 toán tử so sánh 
+var a=2;
+var b=1;
+if(a==b){
+    alert('DUng');
+}
+else{
+    alert('sai');
+}
+
+// toán tử logic
+var a=2;
+b=1;
+if(a>0&& b>0){
+    alert('a và b lơn hon 0')
+}
